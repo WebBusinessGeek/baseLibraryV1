b983b93582c59a15247745e39bbac4693f90ab6f
Kevin Washington <kevw12188@gmail.com>
2015-04-06 14:27:30 -0400

    refactored baseModel class by adding a stringValidator trait to separate responsibility

--- app/base/BaseModel.php
+++ app/base/BaseModel.php
@@ -13,10 +13,8 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseModel extends Model{
 
-    use ModelAttributeManagement;
+    use ModelAttributeManagement, StringValidator;
     
-    protected $invalidCharactersForStringValidation = "/[$%^&*()\-_+={}|\\[\]:;\"'<>?,.\/]/";
-
     protected $primaryOwnerClassName;
 
     protected $multiOwnerClassNames = [];
@@ -50,47 +48,6 @@ abstract class BaseModel extends Model{
         return $valuesWhereKeysWereMatched;
     }
 
-    /**Checks if string is valid.
-     * Returns TRUE if it is a valid string.
-     * Returns FALSE if its not.
-     * THROWS an EXCEPTION if the value passed is not a string.
-     * @param $stringToCheck
-     * @return bool
-     * @throws \Exception
-     */
-    public function stringIsValid($stringToCheck)
-    {
-        if(!is_string($stringToCheck))
-        {
-            throw new \Exception('Argument passed is not a string.');
-        }
-        return(!$this->isInvalidCharactersPresentInString($stringToCheck))? :false;
-    }
-
-    /**Checks if invalid Characters are present in string.
-     * Returns TRUE if invalid characters are detected.
-     * Returns FALSE if NO invalid characters are detected.
-     * Returns TRUE if MODEL has NULL set on the $invalidCharactersForStringValidation property.
-     * @param $stringToCheck
-     * @return bool
-     */
-    public function isInvalidCharactersPresentInString($stringToCheck)
-    {
-        $invalidCharacters = $this->getInvalidCharactersForStringValidation();
-        if(is_null($invalidCharacters))
-        {
-            return true;
-        }
-        return (preg_match_all($invalidCharacters, $stringToCheck) > 0) ? :false;
-    }
-
-    /**Returns invalidCharactersForStringValidation property value.
-     * @return string
-     */
-    public function getInvalidCharactersForStringValidation()
-    {
-        return $this->invalidCharactersForStringValidation;
-    }
 
     public function getSelfPrimaryOwnerClassName()
     {
---
+++ app/base/StringValidator.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 4/6/15
+ * Time: 2:25 PM
+ */
+
+namespace Base;
+
+
+trait StringValidator {
+
+    protected $invalidCharactersForStringValidation = "/[$%^&*()\-_+={}|\\[\]:;\"'<>?,.\/]/";
+
+    /**Checks if string is valid.
+     * Returns TRUE if it is a valid string.
+     * Returns FALSE if its not.
+     * THROWS an EXCEPTION if the value passed is not a string.
+     * @param $stringToCheck
+     * @return bool
+     * @throws \Exception
+     */
+    public function stringIsValid($stringToCheck)
+    {
+        if(!is_string($stringToCheck))
+        {
+            throw new \Exception('Argument passed is not a string.');
+        }
+        return(!$this->isInvalidCharactersPresentInString($stringToCheck))? :false;
+    }
+
+    /**Checks if invalid Characters are present in string.
+     * Returns TRUE if invalid characters are detected.
+     * Returns FALSE if NO invalid characters are detected.
+     * Returns TRUE if MODEL has NULL set on the $invalidCharactersForStringValidation property.
+     * @param $stringToCheck
+     * @return bool
+     */
+    public function isInvalidCharactersPresentInString($stringToCheck)
+    {
+        $invalidCharacters = $this->getInvalidCharactersForStringValidation();
+        if(is_null($invalidCharacters))
+        {
+            return true;
+        }
+        return (preg_match_all($invalidCharacters, $stringToCheck) > 0) ? :false;
+    }
+
+    /**Returns invalidCharactersForStringValidation property value.
+     * @return string
+     */
+    public function getInvalidCharactersForStringValidation()
+    {
+        return $this->invalidCharactersForStringValidation;
+    }
+
+}
